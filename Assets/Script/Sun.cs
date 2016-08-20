ee80fcea502bc9c37351afe235cd635d4bbb40ec
JoJorge <jorge13jorge13@gmail.com>
2016-08-21 01:38:55 +0800

    fix rigidBody FUCKING bug and adjust obj size

--- Assets/Script/Sun.cs
+++ Assets/Script/Sun.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Sun : Entity{
 	void Update(){
 		Quaternion rotation = transform.rotation;
-		rotation.eulerAngles = new Vector3(0, 0, Time.time*25);
+		rotation.eulerAngles = new Vector3(0, 0, Time.time*5);
 		transform.rotation = rotation;
 	}
 }
