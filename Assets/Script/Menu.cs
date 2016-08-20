f92a83b48d4f04af73d0ad394488c9a41dfa072d
GazzaLiu <a608gary@gmail.com>
2016-08-20 12:08:29 +0800

    complete start menu

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public string loadScene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame () {
+		print ("StartGame");
+		//SceneManager.LoadScene (loadScene);
+	}
+
+
+	public void ExitGame () {
+		print ("ExitGame");
+		Application.Quit();
+	}
+		
+}
